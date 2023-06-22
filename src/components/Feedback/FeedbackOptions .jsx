cd80fa88169a70903cb8f63d3195bf36d55e7c4a
OlegatorIT <podlesnyoleg93@gmail.com>
2023-06-22 08:09:54 +0200

    change type

--- src/components/Feedback/FeedbackOptions .jsx
+++ src/components/Feedback/FeedbackOptions .jsx
@@ -5,13 +5,13 @@ import css from './Feedback.module.css';
 export const FeedbackOptions = ({ evt }) => {
   return (
     <div className={css.buttonWrapper} onClick={evt}>
-      <button name="good" type={css.button} className={css.button}>
+      <button name="good" type="button" className={css.button}>
         Good
       </button>
-      <button name="neutral" type={css.button} className={css.button}>
+      <button name="neutral" type="button" className={css.button}>
         Neutral
       </button>
-      <button name="bad" type={css.button} className={css.button}>
+      <button name="bad" type="button" className={css.button}>
         Bad
       </button>
     </div>
